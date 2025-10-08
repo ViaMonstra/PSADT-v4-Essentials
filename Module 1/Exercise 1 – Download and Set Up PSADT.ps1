e97cf5becb50e962b0f8e4c51a9020290462dc7f
arwidmark <arwidmark@gmail.com>
2025-10-08 00:43:38 -0500

    M1

---
+++ Module 1/Exercise 1 – Download and Set Up PSADT.ps1
@@ -0,0 +1,3 @@
+﻿# Exercise 1 – Download and Set Up PSADT
+Set-ExecutionPolicy RemoteSigned -Scope Process
+Install-Module -Name PSAppDeployToolkit -Scope CurrentUser
---
+++ Module 1/Exercise 2 – Create Your First Package.ps1
@@ -0,0 +1,24 @@
+﻿# Exercise 2 – Create Your First Package
+New-ADTTemplate -Destination C:\Apps -Name "FirstPackage"
+
+# App data
+AppVendor = 'Igor Pavlov'
+AppName = '7-Zip'
+AppVersion = '25.01' 
+AppArch = 'x64'
+
+# Install command
+Start-ADTProcess -FilePath '7z2501-x64.exe' -ArgumentList '/S'
+
+# Uninstall command
+Start-ADTProcess -FilePath "$env:ProgramFiles\7-Zip\Uninstall.exe" -ArgumentList '/S' 
+
+# Start package install
+.\Invoke-AppDeployToolkit.exe -DeploymentType Install -DeployMode Interactive
+
+# Start package uninstall
+.\Invoke-AppDeployToolkit.exe -DeploymentType Uninstall
+
+
+
+
